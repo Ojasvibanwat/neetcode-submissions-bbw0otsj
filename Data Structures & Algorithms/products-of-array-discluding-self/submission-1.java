007f34eb2889a4c630a9fd0e134becda12da9e0f
Ojasvi Banwat <62606034+Ojasvibanwat@users.noreply.github.com>
2026-04-05 20:01:36 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product = 1;
+        int zeroCount = 0;
+        for(int num : nums){
+            // if(zeroCount >1){
+            //     product = 0;
+            //     break;
+            // }
+            if(num == 0) {
+                zeroCount ++;
+            }
+            else {
+                product *=num;
+            }
+        }
+
+        if(zeroCount>1) {
+            return new int[nums.length];
+        }
+
+        int[] res = new int[nums.length];
+        
+        for(int i = 0; i<nums.length; i++) {
+            if (zeroCount > 0) {
+                res[i] = (nums[i] == 0) ? product : 0;
+            } else {
+                res[i] = product / nums[i];
+            }
+        }
+        return res;
+    }
+}  
